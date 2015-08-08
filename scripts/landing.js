78980bb0b87c4f43dfd8d8908f289645af11c601
Joon Choi <jychoi19@gmail.com>
2015-08-08 21:07:34 +0900

    Checkpoint 23 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,25 @@
+var pointsArray = document.getElementsByClassName('point');
 
-            var animatePoints = function() {
- 
-                var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
                 
-                var revealPoint = function() {
-                    for (var i = 0; i < points.length; i++) {
-                     points[i].style.opacity = 1;
-                     points[i].style.transform = "scaleX(1) translateY(0)";
-                     points[i].style.msTransform = "scaleX(1) translateY(0)";
-                     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";   
-                    };
-                };
+    var revealPoint = function() {
+        for (var i = 0; i < points.length; i++) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";   
+        };
+    };
+    revealPoint();
+};
 
-                 revealPoint();
- 
-             };
+ window.onload = function() {
+        if (window.innerHeight > 950) {
+         aniatePoints(pointsArray);
+        }
+    window.addEventListener('scroll', function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+            animatePoints(pointsArray);
+         }
+    });
+ }
