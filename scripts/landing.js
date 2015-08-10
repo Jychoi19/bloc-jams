e6439fd5954d7e746a1f8b4180f9483b5096f948
Joon Choi <jychoi19@gmail.com>
2015-08-10 21:45:58 +0900

    Assignment 23 Updated

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,12 +7,13 @@ var animatePoints = function(points) {
             point.style.msTransform = "scaleX(1) translateY(0)";
             point.style.WebkitTransform = "scaleX(1) translateY(0)";   
     };
-forEach(pointsArray, revealPoint);
+//forEach(pointsArray, revealPoint);
+[].forEach.apply(pointsArray, revealPoint);
 };
 
  window.onload = function() {
         if (window.innerHeight > 950) {
-         aniatePoints(pointsArray);
+         animatePoints(pointsArray);
         }
     window.addEventListener('scroll', function(event) {
         if (pointsArray[0].getBoundingClientRect().top <= 500) {
